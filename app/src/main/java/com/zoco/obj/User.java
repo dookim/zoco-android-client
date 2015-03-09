8d6efd6797a1157f8db72488ccfc16fc6c1d5bd3
dookim <dookim@dookims-MacBook-Pro.local>
2015-03-09 20:38:51 +0900

    change user object in order to change nickname

--- app/src/main/java/com/zoco/obj/User.java
+++ app/src/main/java/com/zoco/obj/User.java
@@ -5,15 +5,52 @@ package com.zoco.obj;
  */
 public class User {
 
+    public String email;
     public String nickName;
     public String provider;
     public String univ;
+    public String password;
     public String id;
 
-    public User(String nickName, String univ, String provider, String id) {
+
+    /**
+     * @param nickName
+     * in order to check whether client is registered or not
+     */
+    public User(String nickName) {
         this.nickName = nickName;
-        this.univ = univ;
-        this.provider = provider;
-        this.id = id;
     }
+
+    /**
+     * @param nickName
+     * in order to login, if client login succeed, client will receive "user object"
+     * u can use this object using gson
+     * if fail, client will receive "cannot login" string
+     */
+    public User(String nickName, String password) {
+        this.nickName = nickName;
+        this.password = password;
+    }
+
+    /**
+     *
+     * @param email
+     * @param nickName
+     * @param provider
+     * @param univ
+     * @param password
+     * in order to register user to server, client should this information below
+     * whether client is registered or not, server always send "success"
+     */
+    public User(String email, String nickName, String provider, String univ, String password) {
+        this.email = email;
+        this.nickName = nickName;
+        this.provider = provider;
+        this.univ = univ;
+        this.password = password;
+    }
+
+
+
+
 }
